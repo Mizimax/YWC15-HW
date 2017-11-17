1a634731e65043a3dfa89b9e2d178a23e74c0b21
Your Name <you@example.com>
2017-11-18 03:19:05 +0700

    config global app file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AnnouncementComponent } from './components/announcement/announcement.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HeaderComponent,
+    AnnouncementComponent
   ],
   imports: [
     BrowserModule
